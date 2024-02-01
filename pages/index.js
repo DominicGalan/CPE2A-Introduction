7c46e402d376b209c1f0c6d2ae29d4216b46ecab
DominicGalan <ndpgalan.student@ua.edu.ph>
2024-02-01 14:05:04 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
           I take up the course of computer engineering because it has a high salary rate and I'm the only one who chooses the computer engineering in my family.
           I see myself in 5 years being a CEO of the Nijisanji EN and part of the higher company in Japan and have a happy family.</p>
         <p>
-          (A strong grasp and command of the fundamentals of web development, including HTML, CSS, JavaScript, and pertinent frameworks (such as React, Angular, and Vue).{' '}
+          A strong grasp and command of the fundamentals of web development, including HTML, CSS, JavaScript, and pertinent frameworks (such as React, Angular, and Vue).{' '}
           The capacity to debug code efficiently and understand and solve complicated issues. {' '}
-          Understanding of online security best practices and concepts, including input validation, safe authentication, and defense against common vulnerabilities (e.g., SQL Injection, Cross-Site Scripting).)
+          Understanding of online security best practices and concepts, including input validation, safe authentication, and defense against common vulnerabilities (e.g., SQL Injection, Cross-Site Scripting).
         </p>
       </section>
     </Layout>
